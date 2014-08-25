9ab464663e3ed34969a68440ce1547937fe9c9bc
Adrien Lemaire <dori@ikkyotech.com>
2014-08-25 17:45:22 +0900

    fix typo bash!=zsh

--- memrise_stats.sh
+++ memrise_stats.sh
@@ -24,7 +24,7 @@ scrapy crawl memrise -o $JSON_FILE --logfile=/var/log/memrise.log
 ./get_stats.py $JSON_FILE
 
 
-if [ "$1" == "--no-git" ]
+if [[ "$1" == "--no-git" ]]
 then
     cd ${PROJECT_DIR}kanji_learnt.github.io/ && gCo data/memrise_global_stats.json
     exit 0;
