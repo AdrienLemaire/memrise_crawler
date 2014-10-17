a0a7d3fbf239041980d5519b8d6361c335afbc55
Adrien Lemaire <dori@ikkyotech.com>
2014-10-17 09:11:22 +0900

    update repo after memrise premium support

--- memrise/spiders/memrise_spider.py
+++ memrise/spiders/memrise_spider.py
@@ -2,6 +2,7 @@ from urlparse import urljoin
 import scrapy
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
+from loginform import fill_login_form
 
 from memrise.items import MemriseItem
 from memrise.settings import USER, PASSWORD
@@ -17,19 +18,21 @@ class MemriseSpider(CrawlSpider):
         Rule(LinkExtractor(
             allow='login',
         ), callback='do_login'),
-        Rule(LinkExtractor(
-            restrict_xpaths=('//div[contains(@class, "pinned-courses")]/div/div/div[contains(@class, "detail")]')
-        ), callback='parse_level'),
     )
 
     def do_login(self, response):
-        return scrapy.FormRequest.from_response(
-            response,
-            formdata={'username': USER, 'password': PASSWORD},
-        )
+        args, url, method = fill_login_form(response.url, response.body,
+            USER, PASSWORD)
+        return scrapy.FormRequest(url, method=method, formdata=args,
+                callback=self.parse_dashboard)
+
+    def parse_dashboard(self, response):
+        # If the course has several level, scrape them
+        pagination_selector = '//div[contains(@class, "title")]/a/@href'
+        for url in response.xpath(pagination_selector).extract():
+            yield scrapy.Request(urljoin('http://www.memrise.com', url), callback=self.parse_level)
 
     def parse_level(self, response):
-        # If the course has several level, scrape them
         pagination_selector = '//div[contains(@class, "levels")]/a/@href'
         for url in response.xpath(pagination_selector).extract():
             yield scrapy.Request(urljoin('http://www.memrise.com', url), callback=self.parse_level)
