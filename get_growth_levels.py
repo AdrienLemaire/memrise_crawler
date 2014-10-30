d4a567e3f75cd37918693ced26f9352ee8ebc0fa
Adrien Lemaire <dori@ikkyotech.com>
2014-10-30 22:52:51 +0900

    save items data in json

--- get_growth_levels.py
+++ get_growth_levels.py
@@ -5,23 +5,33 @@
 import argparse
 import json
 import math
+import os.path
 import requests
 
 from local_settings import USERNAME, PASSWORD
+
+INPUT_FILE = os.path.join('/tmp', 'memrise_items.json')
+ALL_ITEMS_FILE = os.path.join(
+    os.path.dirname(__file__),
+    '..', 'kanji_learnt.github.io',
+    'data', 'memrise_all_items.json')
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='get growth levels')
     parser.add_argument('-i', action="store", dest='input',
-        default='memrise_items.json', help='json file input')
+        default=INPUT_FILE, help='json file input (to get items ids)')
+    parser.add_argument('-o', action="store", dest='output',
+        default=ALL_ITEMS_FILE, help='json file output (to store items data)')
     return parser.parse_args()
 
 
-def main(json_file):
-    with open(json_file, 'r') as f:
+def main(input_file, output_file):
+    with open(input_file, 'r') as f:
         data = json.load(f)
     items = [d['item_id'] for d in data]
     growth_levels = []
+    all_data = []
     with requests.Session() as s:
         url = "https://www.memrise.com/login/"
         s.get(url, verify=True)  # get cookies
@@ -37,7 +47,11 @@ def main(json_file):
             url = 'https://www.memrise.com/api/thing/stats/?thing_ids=[{}]'
             r = s.get(url.format(list_ids))
             data = json.loads(r.content)
+            all_data += data['things']
             growth_levels += [d['growth_level'] for d in data['things']]
+
+    with open(output_file, 'w+') as f:
+        f.write(json.dumps(all_data, indent=4))
 
     print "List of my Memrise items levels"
     for i in range(min(growth_levels), max(growth_levels)):
@@ -47,4 +61,4 @@ def main(json_file):
 
 if __name__ == '__main__':
     args = parse_args()
-    main(args.input)
+    main(args.input, args.output)
